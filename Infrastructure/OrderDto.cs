1b649b1069f87d7cf773b22c2a5a7f139d5b34e1
Daniel Okello <handsdani@gmail.com>
2021-07-30 02:38:02 +0300

    creating the OrderDto

---
+++ Infrastructure/OrderDto.cs
@@ -0,0 +1,17 @@
+using CommandCenter.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandCenter.Infrastructure
+{
+    public static class Extensions
+    {
+        public static OrderDto AsDto(this Order order)
+        {
+            return new OrderDto(order.Id, order.Adress, order.Quantity, Order.CreatedDate);
+        }
+
+    }
+}
