a0c0d260331161b6978ea6dfa096539f97bb2baa
Daniel Okello <handsdani@gmail.com>
2021-07-30 08:44:54 +0300

    added logic to service settings

--- Setting/ServiceSettings.cs
+++ Setting/ServiceSettings.cs
@@ -1,7 +1,13 @@
-namespace Pizza_Order_Microservice.Setting
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CommandCenter.Setting
 {
     public class ServiceSettings
     {
-        
+        public string ServiceName { get; init; }
     }
 }
